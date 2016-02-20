32d4ef8257cabdeaf4f090d9cbd7ee57a08e0310
Connor McMahon <connorm333@gmail.com>
2016-02-19 21:53:20 -0600

    Fixed indentation

--- app.js
+++ app.js
@@ -19,54 +19,54 @@ var RIGHT_ARROW = 39;
 //console.log("ayy lmao");
 
 var main = function() {
-	render();
+  render();
 }
 
 var render = function() {
-	context.clearRect(0,0,canvas.width, canvas.height);
-  context.fillStyle = "white";
-	context.fillRect(0,0,w,h);
-	context.strokeStyle = "green";
-	context.strokeRect(0, 0, w, h);
-	drawPlayer();
-  requestAnimationFrame(main);
+    context.clearRect(0,0,canvas.width, canvas.height);
+    context.fillStyle = "white";
+    context.fillRect(0,0,w,h);
+    context.strokeStyle = "green";
+    context.strokeRect(0, 0, w, h);
+    drawPlayer();
+    requestAnimationFrame(main);
 }
 
 var drawPlayer = function() {
-	context.fillStyle = "rgb(0, 255, 0)";
-  context.fillRect(playerX, playerY, playerWidth, playerHeight);
+    context.fillStyle = "rgb(0, 255, 0)";
+    context.fillRect(playerX, playerY, playerWidth, playerHeight);
 }
 
 var playerX = 0;
 var playerY = 0;
 
 var checkPlayerBoundaries = function(x, y, width, height) {
-	if(x < 0){
-  	playerX = 0
-  } else if (x > (width - 1 - playerWidth)) {
-  	playerX = width - 1 - playerWidth;
-  }
-  if(y < 0) {
-  	playerY = 0
-  } else if (y > (height - 1 - playerHeight)) {
-		playerY = height - 1 - playerHeight;
-  }
+    if(x < 0){
+        playerX = 0
+    } else if (x > (width - 1 - playerWidth)) {
+        playerX = width - 1 - playerWidth;
+    }
+    if(y < 0) {
+        playerY = 0
+    } else if (y > (height - 1 - playerHeight)) {
+        playerY = height - 1 - playerHeight;
+    }
 }
 
 document.addEventListener('keydown', function(evt) {
-	switch (evt.keyCode) {
-  case UP_ARROW:
-  	playerY -= playerSpeed;
-    break;
-  case DOWN_ARROW:
-  	playerY += playerSpeed;
-    break;
-  case RIGHT_ARROW:
-  	playerX += playerSpeed;
-    break;
-  case LEFT_ARROW:
-  	playerX -= playerSpeed;
-    break;
+  switch (evt.keyCode) {
+      case UP_ARROW:
+        playerY -= playerSpeed;
+        break;
+      case DOWN_ARROW:
+        playerY += playerSpeed;
+        break;
+      case RIGHT_ARROW:
+        playerX += playerSpeed;
+        break;
+      case LEFT_ARROW:
+        playerX -= playerSpeed;
+        break;
   }
   checkPlayerBoundaries(playerX, playerY, w, h);
 }, false);
